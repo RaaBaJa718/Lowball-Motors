e619cb511efbc50937973fe3638eea00b9b608d8
Ruben <ruben.alston646@gmail.com>
2025-03-31 22:59:07 -0400

    Refactor Header component layout and styles; add navigation links and sign-in functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
+import '../css/Header.css';
 
 function Header() {
   return (
     <header>
-      <div className="menu-icon">
-        <div className="line"></div>
-        <div className="line"></div>
-        <div className="line"></div>
+      <div className="header-top">
+        <div className="header-title">
+          <h1>LowBall Motors</h1>
+        </div>
       </div>
-      <div className="header-title">
-        <h1>LowBall Motors</h1>
-      </div>
-      <div className="header-signin">
-        <div className="signin-icon" title="Sign In"></div>
+      <div className="header-bottom">
+        <nav className="header-menu">
+          <ul className="menu-links">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/inventory">Inventory</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+        <div className="header-signin">
+          <Link to="/login" className="signin-link" title="Sign In">Sign In</Link>
+        </div>
       </div>
     </header>
   );
-}
-
+};
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './styles.css'; // Consolidated styles file
 import App from './App.jsx'
 
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
